2dec52fb4d9a1fb52dd32447faf25c5e71da74e0
WhiteDevil2013 <william.wakim@epita.fr>
2017-07-25 17:35:00 +0200

    finalize subject 2

--- project-1/sujet2.py
+++ project-1/sujet2.py
@@ -1,9 +1,23 @@
+#!/usr/bin/env python
+
 import shutil
 import os
 import sys
+import operator
 from math import *
 
-tri = open(input("Input Files: "))
+files = sys.argv[1:]
+nbTotalFiles = len(files)
+nbFileWithWord = 1
+nbGrams = int(input('Please enter the number of representative ngram you want to see: \n'))
+
+def getTf(countCurWord, countMostFrequent):
+	tf = countCurWord / countMostFrequent
+	return tf
+	
+def getIdf(nbTotalFiles, nbFileWithWord):
+	ratio = nbTotalFiles / nbFileWithWord
+	return (log10(ratio) + 1)
 
 def ngrams(input, n):
   input = input.split(' ')
@@ -17,18 +31,44 @@ def replace_all(text):
     for i in sanitize:
         text = text.replace(i, '')
     return text
+	
+gramsOccurence = {}
+gramsFile = {}
+resultString = ''
+countMostFrequent = 0
 
-ngramList = []
-grams = {}
-for f in tri:
-	data = f.strip()
+print('processing...')
+
+for file in files:
+	tmpSet = {}
+	f = open(file)
+	data = f.read().strip()
 	data = replace_all(data)
 	data = data.lower()
 	for r in range(2, 7):
 		ngs = ngrams(data, r)
 		ngs = [' '.join(x) for x in ngs]
 		for g in ngs:
-			grams.setdefault(g, 0)
-			grams[g] += 1
-ngramList.append(grams)
-print(ngramList)
+			gramsOccurence.setdefault(g, 0)
+			gramsOccurence[g] += 1
+			countMostFrequent = max(gramsOccurence[g], countMostFrequent)
+			if not g in tmpSet:
+				gramsFile.setdefault(g, 0)
+				gramsFile[g] += 1;
+				tmpSet.setdefault(g, 0)
+				tmpSet[g] += 1;
+
+
+print('finalize')
+tfIdfSet = {}
+for gram in gramsOccurence:
+	tfIdf = getTf(gramsOccurence[gram], countMostFrequent) * getIdf(nbTotalFiles, gramsFile[gram])
+	tfIdfSet[gram] = tfIdf
+
+resultList = sorted(tfIdfSet.items(), key=operator.itemgetter(1), reverse=True)[:nbGrams]
+for res in resultList:
+	resultString += str(res) + '\n'
+
+f = open('result.txt', 'w+')
+print('please check generated result.txt file')
+f.write(resultString)
